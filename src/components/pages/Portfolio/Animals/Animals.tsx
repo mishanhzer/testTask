d4a233bc96f1d038db30af3d1f68b993410573f3
Michael <mishanhzer@gmail.com>
2025-03-19 09:20:46 +0500

    added zustand load more elements

--- src/components/pages/Portfolio/Animals/Animals.tsx
+++ src/components/pages/Portfolio/Animals/Animals.tsx
@@ -1,49 +1,24 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 
-import { AnimalPicture } from "../../../../assets/images/works/animals/animalImages";
+import { useAnimalStore } from '../../../../store/store'
 
-// import { filterAnimals } from "../../../../assets/images/works/allWorks/AllWorks";
-// import { Content } from "./AnimalsWorks";
-// import { Content } from "../../../../assets/images/works/allWorks/AllWorks";
 import { classNamesLinkAndButton } from '../../../UI_kits/stylesUI_kits'
-
-import { useStore } from '../../../../store/store'
-
 import styles from './styles/animals.module.scss'
 
 export const Animals = () => {
-  const displayedData = useStore((state) => state.displayedData)
-  // const displayedData = useStore(state => state.displayedData)
-  const setVisibleData = useStore(state => state.setVisibleData)
-  const setDisplayedData = useStore(state => state.setDisplayedData)
-
-  // const filterAnimals = works.filter(item => item.category === 'animal')
-
-  // const [displayedData, setDisplayedData] = useState(filterAnimals.slice(0, 9))
-  // const [visibleItemCount, setVisibleItemCount] = useState(9)
-
-  // const handleLoadMore = () => {
-  //   setVisibleItemCount(prevCount => prevCount + 9)
-  //   setDisplayedData(filterAnimals.slice(0, visibleItemCount + 9))
-  // }
-
-  const handleLoadMore = () => {
-    setVisibleData()
-    setDisplayedData()
-  }
-
-  // console.log(displayedData.length - 1)
-  // console.log(displayedData[displayedData.length - 1])
+  const animalWorks = useAnimalStore(state => state.animalWorks)
+  const animalDisplayedData = useAnimalStore(state => state.animalDisplayedData)
+  const handleAnimalLoadMore = useAnimalStore(state => state.handleAnimalLoadMore)
 
   return (
     <>
       <div className={styles.container}>
-        {displayedData.map((item, index) => (
+        {animalDisplayedData.map((item, index) => (
           <img className={`w-56 h-56 lozad`} src={item.source} key={index} alt={item.name} />
         ))}
         <button
-          // disabled={displayedData[displayedData.length - 1].category === 'animal' && displayedData[displayedData.length - 1].id === displayedData.length - 1 ? true : false}
-          onClick={handleLoadMore}
+          disabled={animalDisplayedData[animalDisplayedData.length - 1].category === 'animal' && animalDisplayedData.length - 1 === animalWorks.length - 1 ? true : false}
+          onClick={handleAnimalLoadMore}
           className={`${styles.btn} ${classNamesLinkAndButton} mt-8 px-4 w-48 justify-center text-3xl`}
         >Load More
         </button>
--- src/store/store.ts
+++ src/store/store.ts
@@ -8,37 +8,67 @@ import {
 } from "../assets/images/works/allWorks/AllWorks";
 
 interface TypesStore {
-  // works: TypesDataWorks[]
-  displayedData: TypesDataWorks[]
-  visibleData: number
-  setVisibleData: () => void
-  setDisplayedData: () => void
+  animalWorks: TypesDataWorks[]
+  animalDisplayedData: TypesDataWorks[]
+  animalVisibleData: number
+  setAnimalVisibleData: () => void
+  setAnimalDisplayedData: () => void
+  funcForCall: () => void
+  handleAnimalLoadMore: () => void
 }
 
-console.log(dataWorks)
-
-export const useStore = create<TypesStore>()(
+export const useAnimalStore = create<TypesStore>()(
   devtools(
     immer(
-      persist(
         (set) => ({
-          displayedData: dataWorks.filter(item => item.category === 'animal').slice(0, 9),
-          visibleData: 9,
-          setVisibleData: () => 
+          animalWorks: dataWorks.filter(item => item.category === 'animal'),
+          animalDisplayedData: dataWorks.filter(item => item.category === 'animal').slice(0, 9),
+          animalVisibleData: 9,
+          funcForCall: () => {},
+          setAnimalVisibleData: () => 
             set((state) => ({
-              visibleData: state.visibleData + 9
+              animalVisibleData: state.animalVisibleData + 9
             })),
-          setDisplayedData: () => 
+          setAnimalDisplayedData: () => 
             set((state) => ({
-              displayedData: dataWorks.filter(item => item.category === 'animal').slice(0, 27)
-            }))
+              animalDisplayedData: dataWorks.filter(item => item.category === 'animal').slice(0, state.animalVisibleData)
+            })),
+          handleAnimalLoadMore: () => 
+            set((state) => ({
+              funcForCall: [state.setAnimalVisibleData(), state.setAnimalDisplayedData()]
+            })),
         }),
-        {
-          // options (настройки storage)
-          name: "work-storage",
-        }
-      )
     )
   )
 );
+
+
+
+
+// Cтарый код с persist
+// export const useStore = create<TypesStore>()(
+//   devtools(
+//     immer(
+//       persist(
+//         (set) => ({
+//           displayedData: dataWorks.filter(item => item.category === 'animal').slice(0, 9),
+//           visibleData: 9,
+//           setVisibleData: (prevCount) => 
+//             set((state) => ({
+//               visibleData: state.visibleData + 9
+//             })),
+//           setDisplayedData: () => 
+//             set((state) => ({
+//               displayedData: dataWorks.filter(item => item.category === 'animal').slice(0, state.visibleData)
+//             }))
+//         }),
+//         {
+//           // options (настройки storage)
+//           name: "work-storage",
+//         }
+//       )
+//     )
+//   )
+// );
+
 
