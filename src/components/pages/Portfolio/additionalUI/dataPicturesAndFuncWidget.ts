8e615722ca3357744af62f14bdb7d58942f06770
Michael <mishanhzer@gmail.com>
2025-04-25 13:46:54 +0500

    add func for styles pictures

--- src/components/pages/Portfolio/additionalUI/dataPicturesAndFuncWidget.ts
+++ src/components/pages/Portfolio/additionalUI/dataPicturesAndFuncWidget.ts
@@ -48,3 +48,107 @@ export const stillLifeDataPages = (path: string, styles: string, setOnePage: Typ
     { path: `${path}2`, name: 2, source: '', class: styles, func: setTwoPage },
   ]
 }
+
+export const positionModalTest = (style: string, position: string) => {
+    switch (position) {
+      case 'akitoInu.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'beagle.jpg': return `${style} scale-[1.4]`
+      case 'blueAndYellowMacaw.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'bluebirdsOnFloweringBranch.jpg': return `${style} scale-[1.4]`
+      case 'cat.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'catMischievous.jpg': return `${style} scale-[1.4]`
+      case 'catWithGreenEyes.jpg': return `${style} scale-[1.4]`
+      case 'catsInLibrary.jpg': return `${style} scale-[1.4]`
+      case 'domesticCat.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'flight.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'fluffyJoy.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'fox.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'goldfish.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'greenWingedMacaw.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'hedgehog.jpg': return `${style} mt-[-75px] scale-[1.4]`
+      case 'horse.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'horseAndFoal.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'junonia.jpg': return `${style} scale-[1.4]`
+      case 'ladybugOnForgetMeNots.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'ladybugs.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'leopard.jpg': return `${style} scale-[1.4]`
+      case 'leopardAtWateringHole.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'leopardsAtSunset.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'leopardsGaze.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'lionFamily.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'lionessAndLionCub.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'littleRedPanda.jpg': return `${style} scale-[1.4]`
+      case 'lynx.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'makhaon.jpg': return `${style} scale-[1.4]`
+      case 'monarch.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'ocelot.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'peacocksEye.jpg': return `${style} scale-[1.4]`
+      case 'pomeranian.jpg': return `${style} scale-[1.4]`
+      case 'puma.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'redCatOnTheFence.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'redHorse.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'snailOnStrawberry.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'snowLeopard.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'spaniel.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'squirrel.jpg': return `${style} scale-[1.4]`
+      case 'swans.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'tiger.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'tigerGaze.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'tigerHost.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'tigerInTheTicket.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'toyger.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'whiteEagle.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'wolf.jpg': return `${style} mt-[-150px] scale-[1.2]`
+
+      case 'blueIrises.jpg': return `${style} mt-[-175px] scale-[1.1]`
+      case 'branchOfLilac.jpg': return `${style} mt-[-50px] scale-[1.4]`
+      case 'brightPeonies.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'brightTulips.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'delicateRose.jpg': return `${style} mt-[-100px] scale-[1.4]`
+      case 'delicateScentOfPeonies.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'delicateTulips.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'eveningFlowers.jpg': return `${style} mt-[-175px] scale-[1.1]`
+      case 'flowersForBeloved.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'flowersMood.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'irises.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'largePeonies.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'luxuriousPeonies.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'narcissists.jpg': return `${style} mt-[-50px] scale-[1.4]`
+      case 'peoniesByTheWindow.jpg': return `${style} scale-[1.4]`
+      case 'peonyFestival.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'pinkBlooming.jpg': return `${style} scale-[1.4]`
+      case 'pinkPeonies.jpg': return `${style} scale-[1.4]`
+      case 'pinkTenderness.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'redFowersAtSunset.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'roseInGarden.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'yellowRose.jpg': return `${style} scale-[1.4]`
+
+      case 'daisiesOnOldSuitcase.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'delicateHydrangea.jpg': return `${style} mt-[-125px] scale-[1.2]`
+      case 'grapefruits.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'hydrangea.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'lemonsOrangesLimes.jpg': return `${style} scale-[1.4]`
+      case 'orangeJuiceFresh.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'peoniesByTheWindow.jpg': return `${style} scale-[1.4]`
+      case 'stillLifeWithBookAndKeys.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'stillLifeWithChamomileAndStrawberry.jpg': return `${style} scale-[1.4]`
+      case 'stillLifeWithChrysanthemumsAndFruits.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'stillLifeWithFigsAndGrapes.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'stillLifeWithGreenGrapes.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'stillLifeWithLemons.jpg': return `${style} scale-[1.4]`
+      case 'stillLifeWithPeach.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'stillLifeWithPomegranates.jpg': return `${style} mt-[-175px] scale-[1.2]`
+      case 'stillLifeWithSamovar.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'wildflowersInAVase.jpg': return `${style} scale-[1.4]`
+      case 'wineAndFruits.jpg': return `${style} mt-[-175px] scale-[1.2]`
+
+      case 'belovedBaby.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'bestFriend.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'girlAndCat.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'girlAndDucklings.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'girlWithDog.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'girlWithHorse.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'letsSingMyFriend.jpg': return `${style} mt-[-150px] scale-[1.2]`
+      case 'theGirlWithTheWhiteRabbits.jpg': return `${style} mt-[-150px] scale-[1.2]`
+    }
+  }
--- src/components/pages/Portfolio/additionalUI/picturesContent/Modal.tsx
+++ src/components/pages/Portfolio/additionalUI/picturesContent/Modal.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
-import React, { useRef, useState, useEffect, useMemo, useLayoutEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { AnimationSinglePicture } from '../dataPicturesAndFuncWidget'
+
+import { positionModalTest } from '../dataPicturesAndFuncWidget';
 
 import styles from '../../styles/mainStylesPictures.module.scss'
 
@@ -13,16 +15,7 @@ interface TypesModalPortal {
 }
 
 export const ModalPortal = ({ position, handleClose, source, alt }: TypesModalPortal) => {
-  const [heightImage, setHeightImage] = useState<number>(0)
   const ref = useRef(null)
-
-  useLayoutEffect(() => {
-    if (ref.current) {
-      setHeightImage((ref.current as unknown as HTMLElement).getBoundingClientRect().height);
-    }
-  }, [])
-
-  const portalDiv = document.getElementById('modal-root')!;
 
   useEffect(() => {
     const closeModal = (e: KeyboardEvent) => {
@@ -36,36 +29,7 @@ export const ModalPortal = ({ position, handleClose, source, alt }: TypesModalPo
     }
   }, [])
 
-  // const handleClass = useMemo(() => {
-  //   if (heightImage > 0) {
-  //     if (heightImage < 350) {
-  //       return `${styles.mainWrapperStyle} scale-[1.2]`
-  //     }
-  //     if (heightImage > 350 && heightImage <= 450) {
-  //       return `${styles.mainWrapperStyle} scale-[1.2]`
-  //     }
-  //     if (heightImage > 600 && heightImage < 700) {
-  //       return `${styles.mainWrapperStyle} scale-[1.2]`
-  //     }
-  //     else {
-  //       return ${styles.mainWrapperStyle}` scale-[1.2]`
-  //     }
-  //   } else {
-  //     return styles.mainWrapperStyle;
-  //   }
-  // }, [heightImage])
-
-  console.log(position)
-  const positionModal = useMemo(() => {
-    switch (position) {
-      case 'akitoInu.jpg':
-        return `${styles.mainWrapperStyle} mt-[-150px]`
-      case 'beagle.jpg':
-        return `${styles.mainWrapperStyle}`
-      case 'blueAndYellowMacaw.jpg':
-        return `${styles.mainWrapperStyle} mt-[-150px]`
-    }
-  }, [])
+  const portalDiv = document.getElementById('modal-root')!;
 
   return ReactDOM.createPortal(
     <AnimationSinglePicture>
@@ -74,7 +38,7 @@ export const ModalPortal = ({ position, handleClose, source, alt }: TypesModalPo
           onClick={() => handleClose()}
           className={`${styles.overlay}`}>
           <div onClick={(e) => e.stopPropagation()}
-            className={positionModal}
+            className={positionModalTest(styles.mainWrapperStyle, position)}
           >
             <button onClick={handleClose} className={styles.close} />
             <img ref={ref} className={`absolute lozad`} src={source} alt={alt} />
